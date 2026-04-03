b757575d36708b8798ff4e8786042b0424f5ee9b
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-04-03 21:55:34 +0530

    Concept of array size bound in c

---
+++ Trail.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ void main()
+ {
+    int A,B[2],C,D,E;
+    A=1;
+    C=1;
+    D=1;
+    E=1;
+    B[0]=2;
+    B[1]=2;
+    B[2]=2;
+    B[3]=2;
+    printf("%d%d%d%d",A,C,D,E);
+ }
