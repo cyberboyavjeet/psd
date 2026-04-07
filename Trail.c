6002d4a6badcc583645416f7f2e8adee98ca6457
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-04-07 13:34:42 +0530

    Fabenoic and Factorial

--- Trail.c
+++ Trail.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+ #include<stdio.h>
  void main()
  {
     int A,B[2],C,D,E;
@@ -6,9 +6,14 @@
     C=1;
     D=1;
     E=1;
-    B[0]=2;
     B[1]=2;
     B[2]=2;
     B[3]=2;
+    B[4]=2;
     printf("%d%d%d%d",A,C,D,E);
+    printf("\n");
+   printf("%u\t%u\t%u",&A,&D,&E);
+   printf("\n");
+    printf("%u\t%u\t%u",&B[1],&B[2],&B[3],B[4]);
+
  }
